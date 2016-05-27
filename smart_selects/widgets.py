5d47fba47ad5a60b0f15b7419e351eca73f512d3
Kara <moarcant@users.noreply.github.com>
2016-05-27 10:58:55 -0500

    Update widgets.py

--- smart_selects/widgets.py
+++ smart_selects/widgets.py
@@ -18,46 +18,55 @@ except ImportError:
     from django.apps.apps import get_model
 
 
-if django.VERSION >= (1, 2, 0) and getattr(settings,
-                                           'USE_DJANGO_JQUERY', True):
+if django.VERSION >= (1, 2, 0) and getattr(settings, 'USE_DJANGO_JQUERY', True):
     USE_DJANGO_JQUERY = True
 else:
     USE_DJANGO_JQUERY = False
-    JQUERY_URL = getattr(settings, 'JQUERY_URL', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js')
+    JQUERY_URL = getattr(settings, 'JQUERY_URL', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js')
 
 URL_PREFIX = getattr(settings, "SMART_SELECTS_URL_PREFIX", "")
 
 
 class ChainedSelect(Select):
-    def __init__(self, app_name, model_name, chain_field,
-                 model_field, show_all, auto_choose,
-                 manager=None, view_name=None, *args, **kwargs):
-        self.app_name = app_name
-        self.model_name = model_name
-        self.chain_field = chain_field
-        self.model_field = model_field
+    def __init__(self, to_app_name, to_model_name, chained_field, chained_model_field,
+                 foreign_key_app_name, foreign_key_model_name, foreign_key_field_name,
+                 show_all, auto_choose, manager=None, view_name=None, *args, **kwargs):
+        self.to_app_name = to_app_name
+        self.to_model_name = to_model_name
+        self.chained_field = chained_field
+        self.chained_model_field = chained_model_field
         self.show_all = show_all
         self.auto_choose = auto_choose
         self.manager = manager
         self.view_name = view_name
+        self.foreign_key_app_name = foreign_key_app_name
+        self.foreign_key_model_name = foreign_key_model_name
+        self.foreign_key_field_name = foreign_key_field_name
         super(Select, self).__init__(*args, **kwargs)
 
-    class Media:
+    @property
+    def media(self):
+        """Media defined as a dynamic property instead of an inner class."""
+        vendor = '' if django.VERSION < (1, 9, 0) else 'vendor/jquery/'
         extra = '' if settings.DEBUG else '.min'
         js = [
-            'jquery%s.js' % extra,
-            'jquery.init.js'
+            '%sjquery%s.js' % (vendor, extra),
+            'jquery.init.js',
         ]
         if USE_DJANGO_JQUERY:
             js = [static('admin/js/%s' % url) for url in js]
         elif JQUERY_URL:
             js = [JQUERY_URL]
+        js = js + [static('smart-selects/admin/js/chainedfk.js')]
+
+        return forms.Media(js=js)
 
     def render(self, name, value, attrs=None, choices=()):
         if len(name.split('-')) > 1:  # formset
-            chain_field = '-'.join(name.split('-')[:-1] + [self.chain_field])
+            chained_field = '-'.join(name.split('-')[:-1] + [self.chained_field])
         else:
-            chain_field = self.chain_field
+            chained_field = self.chained_field
+
         if not self.view_name:
             if self.show_all:
                 view_name = "chained_filter_all"
@@ -65,8 +74,15 @@ class ChainedSelect(Select):
                 view_name = "chained_filter"
         else:
             view_name = self.view_name
-        kwargs = {'app': self.app_name, 'model': self.model_name,
-                  'field': self.model_field, 'value': "1"}
+        kwargs = {
+            'app': self.to_app_name,
+            'model': self.to_model_name,
+            'field': self.chained_model_field,
+            'foreign_key_app_name': self.foreign_key_app_name,
+            'foreign_key_model_name': self.foreign_key_model_name,
+            'foreign_key_field_name': self.foreign_key_field_name,
+            'value': '1'
+            }
         if self.manager is not None:
             kwargs.update({'manager': self.manager})
         url = URL_PREFIX + ("/".join(reverse(view_name, kwargs=kwargs).split("/")[:-2]))
@@ -74,127 +90,47 @@ class ChainedSelect(Select):
             auto_choose = 'true'
         else:
             auto_choose = 'false'
-        empty_label = iter(self.choices).next()[1]  # Hacky way to getting the correct empty_label from the field instead of a hardcoded '--------'
+        iterator = iter(self.choices)
+        if hasattr(iterator, '__next__'):
+            empty_label = iterator.__next__()[1]
+        else:
+            # Hacky way to getting the correct empty_label from the field instead of a hardcoded '--------'
+            empty_label = iterator.next()[1]
+
         js = """
         <script type="text/javascript">
-        //<![CDATA[
         (function($) {
-            function fireEvent(element,event){
-                if (document.createEventObject){
-                // dispatch for IE
-                var evt = document.createEventObject();
-                return element.fireEvent('on'+event,evt)
-                }
-                else{
-                // dispatch for firefox + others
-                var evt = document.createEvent("HTMLEvents");
-                evt.initEvent(event, true, true ); // event type,bubbling,cancelable
-                return !element.dispatchEvent(evt);
-                }
-            }
-
-            function dismissRelatedLookupPopup(win, chosenId) {
-                var name = windowname_to_id(win.name);
-                var elem = document.getElementById(name);
-                if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
-                    elem.value += ',' + chosenId;
-                } else {
-                    elem.value = chosenId;
-                }
-                fireEvent(elem, 'change');
-                win.close();
-            }
-
-            $(document).ready(function(){
-                function fill_field(val, init_value){
-                    if (!val || val==''){
-                        options = '<option value="">%(empty_label)s<'+'/option>';
-                        $("#%(id)s").html(options);
-                        $('#%(id)s option:first').attr('selected', 'selected');
-                        $("#%(id)s").trigger('change');
-                        return;
-                    }
-                    $.getJSON("%(url)s/"+val+"/", function(j){
-                        var options = '<option value="">%(empty_label)s<'+'/option>';
-                        for (var i = 0; i < j.length; i++) {
-                            options += '<option value="' + j[i].value + '">' + j[i].display + '<'+'/option>';
-                        }
-                        var width = $("#%(id)s").outerWidth();
-                        $("#%(id)s").html(options);
-                        if (navigator.appVersion.indexOf("MSIE") != -1)
-                            $("#%(id)s").width(width + 'px');
-                        $('#%(id)s option:first').attr('selected', 'selected');
-                        var auto_choose = %(auto_choose)s;
-                        if(init_value){
-                            $('#%(id)s option[value="'+ init_value +'"]').attr('selected', 'selected');
-                        }
-                        if(auto_choose && j.length == 1){
-                            $('#%(id)s option[value="'+ j[0].value +'"]').attr('selected', 'selected');
-                        }
-                        $("#%(id)s").trigger('change');
-                    })
-                }
-
-                if(!$("#id_%(chainfield)s").hasClass("chained")){
-                    var val = $("#id_%(chainfield)s").val();
-                    fill_field(val, "%(value)s");
-                }
-
-                $("#id_%(chainfield)s").change(function(){
-                    var start_value = $("#%(id)s").val();
-                    var val = $(this).val();
-                    fill_field(val, start_value);
-                })
-            })
-            if (typeof(dismissAddAnotherPopup) !== 'undefined') {
-                var oldDismissAddAnotherPopup = dismissAddAnotherPopup;
-                dismissAddAnotherPopup = function(win, newId, newRepr) {
-                    oldDismissAddAnotherPopup(win, newId, newRepr);
-                    if (windowname_to_id(win.name) == "id_%(chainfield)s") {
-                        $("#id_%(chainfield)s").change();
-                    }
-                }
-            }
+            var chainfield = "#id_%(chainfield)s";
+            var url = "%(url)s";
+            var id = "#%(id)s";
+            var value = %(value)s;
+            var auto_choose = %(auto_choose)s;
+            var empty_label = "%(empty_label)s";
+            $(document).ready(function() {
+                chainedfk.init(chainfield, url, id, value, empty_label, auto_choose);
+            });
         })(jQuery || django.jQuery);
-        //]]>
         </script>
-
         """
-        js = js % {"chainfield": chain_field,
+        js = js % {"chainfield": chained_field,
                    "url": url,
                    "id": attrs['id'],
-                   'value': value,
+                   'value': 'undefined' if value is None or value == '' else value,
                    'auto_choose': auto_choose,
-                   'empty_label': empty_label}
+                   'empty_label': escape(empty_label)}
         final_choices = []
-
         if value:
-            item = self.queryset.filter(pk=value)[0]
-            try:
-                pk = getattr(item, self.model_field + "_id")
-                filter = {self.model_field: pk}
-            except AttributeError:
-                try:  # maybe m2m?
-                    pks = getattr(item, self.model_field).all().values_list('pk', flat=True)
-                    filter = {self.model_field + "__in": pks}
-                except AttributeError:
-                    try:  # maybe a set?
-                        pks = getattr(item, self.model_field + "_set").all().values_list('pk', flat=True)
-                        filter = {self.model_field + "__in": pks}
-                    except:  # give up
-                        filter = {}
-            filtered = list(get_model(self.app_name, self.model_name).objects.filter(**filter).distinct())
-            filtered.sort(cmp=locale.strcoll, key=lambda x: unicode_sorter(unicode(x)))
-            for choice in filtered:
-                final_choices.append((choice.pk, unicode(choice)))
+            available_choices = self._get_available_choices(self.queryset, value)
+            for choice in available_choices:
+                final_choices.append((choice.pk, force_text(choice)))
         if len(final_choices) > 1:
             final_choices = [("", (empty_label))] + final_choices
         if self.show_all:
             final_choices.append(("", (empty_label)))
             self.choices = list(self.choices)
-            self.choices.sort(cmp=locale.strcoll, key=lambda x: unicode_sorter(x[1]))
+            self.choices.sort(key=lambda x: unicode_sorter(x[1]))
             for ch in self.choices:
-                if not ch in final_choices:
+                if ch not in final_choices:
                     final_choices.append(ch)
         self.choices = ()
         final_attrs = self.build_attrs(attrs, name=name)
@@ -202,6 +138,127 @@ class ChainedSelect(Select):
             final_attrs['class'] += ' chained'
         else:
             final_attrs['class'] = 'chained'
-        output = super(ChainedSelect, self).render(name, value, final_attrs, choices=final_choices)
+        
+        output = js
+        output += super(ChainedSelect, self).render(name, value, final_attrs, choices=final_choices)
+        
+        return mark_safe(output)
+
+    def _get_available_choices(self, queryset, value):
+        """
+        get possible choices for selection
+        """
+        item = queryset.filter(pk=value).first()
+        if item:
+            try:
+                pk = getattr(item, self.chained_model_field + "_id")
+                filter = {self.chained_model_field: pk}
+            except AttributeError:
+                try:  # maybe m2m?
+                    pks = getattr(item, self.chained_model_field).all().values_list('pk', flat=True)
+                    filter = {self.chained_model_field + "__in": pks}
+                except AttributeError:
+                    try:  # maybe a set?
+                        pks = getattr(item, self.chained_model_field + "_set").all().values_list('pk', flat=True)
+                        filter = {self.chained_model_field + "__in": pks}
+                    except:  # give up
+                        filter = {}
+            filtered = list(get_model(self.to_app_name, self.to_model_name).objects.filter(**filter).distinct())
+            sort_results(filtered)
+        else:
+            # invalid value for queryset
+            filtered = []
+
+        return filtered
+
+
+class ChainedSelectMultiple(SelectMultiple):
+    def __init__(self, to_app_name, to_model_name, chain_field, chained_model_field,
+                 foreign_key_app_name, foreign_key_model_name, foreign_key_field_name,
+                 auto_choose, manager=None, *args, **kwargs):
+        self.to_app_name = to_app_name
+        self.to_model_name = to_model_name
+        self.chain_field = chain_field
+        self.chained_model_field = chained_model_field
+        self.auto_choose = auto_choose
+        self.manager = manager
+        self.foreign_key_app_name = foreign_key_app_name
+        self.foreign_key_model_name = foreign_key_model_name
+        self.foreign_key_field_name = foreign_key_field_name
+
+        super(SelectMultiple, self).__init__(*args, **kwargs)
+
+    @property
+    def media(self):
+        """Media defined as a dynamic property instead of an inner class."""
+        vendor = '' if django.VERSION < (1, 9, 0) else 'vendor/jquery/'
+        extra = '' if settings.DEBUG else '.min'
+        js = [
+            '%sjquery%s.js' % (vendor, extra),
+            'jquery.init.js',
+        ]
+        if USE_DJANGO_JQUERY:
+            js = [static('admin/js/%s' % url) for url in js]
+        elif JQUERY_URL:
+            js = [JQUERY_URL]
+        js = js + [static('smart-selects/admin/js/chainedm2m.js')]
+
+        return forms.Media(js=js)
+
+    def render(self, name, value, attrs=None, choices=()):
+        if len(name.split('-')) > 1:  # formset
+            chain_field = '-'.join(name.split('-')[:-1] + [self.chain_field])
+        else:
+            chain_field = self.chain_field
+
+        view_name = 'chained_filter'
+
+        kwargs = {
+            'app': self.to_app_name,
+            'model': self.to_model_name,
+            'field': self.chained_model_field,
+            'foreign_key_app_name': self.foreign_key_app_name,
+            'foreign_key_model_name': self.foreign_key_model_name,
+            'foreign_key_field_name': self.foreign_key_field_name,
+            'value': '1'
+            }
+        if self.manager is not None:
+            kwargs.update({'manager': self.manager})
+        url = URL_PREFIX + ("/".join(reverse(view_name, kwargs=kwargs).split("/")[:-2]))
+        if self.auto_choose:
+            auto_choose = 'true'
+        else:
+            auto_choose = 'false'
+        js = """
+        <script type="text/javascript">
+        (function($) {
+        var chainfield = "#id_%(chainfield)s";
+        var url = "%(url)s";
+        var id = "#%(id)s";
+        var value = %(value)s;
+        var auto_choose = %(auto_choose)s;
+        $(document).ready(function() {
+            chainedm2m.init(chainfield, url, id, value, auto_choose);
+        });
+        })(jQuery || django.jQuery);
+        </script>
+        """
+        js = js % {"chainfield": chain_field,
+                   "url": url,
+                   "id": attrs['id'],
+                   'value': json.dumps(value),
+                   'auto_choose': auto_choose}
+
+        # since we cannot deduce the value of the chained_field
+        # so we just render empty choices here and let the js
+        # fetch related choices later
+        final_choices = []
+        self.choices = ()  # need to set explicitly because the Select widget will use it in render
+        final_attrs = self.build_attrs(attrs, name=name)
+        if 'class' in final_attrs:
+            final_attrs['class'] += ' chained'
+        else:
+            final_attrs['class'] = 'chained'
+        output = super(ChainedSelectMultiple, self).render(name, value, final_attrs, choices=final_choices)
         output += js
         return mark_safe(output)
